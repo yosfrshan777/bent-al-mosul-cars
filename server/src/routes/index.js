ba4723a8ed051589119a358a4806e15492bf3b4b
yosfrshan777 <yosfrshan777@gmail.com>
2026-08-28 23:01:39 +0300

    Update index.html for Bent Al-Mosul Cars

---
+++ server/src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const authRoutes = require('./auth');
+const carRoutes = require('./cars');
+const adminRoutes = require('./admin');
+const paymentRoutes = require('./payment');
+const showroomRoutes = require('./showrooms');
+const partsRoutes = require('./parts');
+const messageRoutes = require('./messages');
+
+const router = express.Router();
+
+router.use('/auth', authRoutes);
+router.use('/cars', carRoutes);
+router.use('/admin', adminRoutes);
+router.use('/payment', paymentRoutes);
+router.use('/showrooms', showroomRoutes);
+router.use('/parts', partsRoutes);
+router.use('/messages', messageRoutes);
+
+module.exports = router;
