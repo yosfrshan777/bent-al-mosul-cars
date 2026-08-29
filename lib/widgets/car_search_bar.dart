04443c73a78360ebbe41d6e649eef74f78758a95
yosfrshan777 <yosfrshan777@gmail.com>
2026-08-29 14:35:30 +0300

    Remove filter control from car search

--- lib/widgets/car_search_bar.dart
+++ lib/widgets/car_search_bar.dart
@@ -5,108 +5,33 @@ class CarSearchBar extends StatelessWidget {
     super.key,
     this.controller,
     this.onChanged,
-    this.onFilter,
     this.hint = 'ابحث عن ماركة أو موديل أو مدينة',
   });
 
   final TextEditingController? controller;
   final ValueChanged<String>? onChanged;
-  final VoidCallback? onFilter;
   final String hint;
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: controller,
-              onChanged: onChanged,
-              textDirection: TextDirection.rtl,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 13,
-              ),
-              decoration: InputDecoration(
-                hintText: hint,
-                hintStyle: const TextStyle(
-                  color: Colors.white38,
-                  fontSize: 12,
-                ),
-                prefixIcon: const Icon(
-                  Icons.search_rounded,
-                  color: Color(0xFFFF176F),
-                  size: 22,
-                ),
-                filled: true,
-                fillColor: const Color(0xFF15151B),
-                contentPadding:
-                    const EdgeInsets.symmetric(
-                  horizontal: 14,
-                  vertical: 15,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius:
-                      BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: Color(0xFF292932),
-                  ),
-                ),
-                enabledBorder:
-                    OutlineInputBorder(
-                  borderRadius:
-                      BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: Color(0xFF292932),
-                  ),
-                ),
-                focusedBorder:
-                    OutlineInputBorder(
-                  borderRadius:
-                      BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: Color(0xFFFF176F),
-                    width: 1.3,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          if (onFilter != null) ...[
-            const SizedBox(width: 9),
-            Material(
-              color: const Color(0xFF15151B),
-              borderRadius:
-                  BorderRadius.circular(15),
-              child: InkWell(
-                onTap: onFilter,
-                borderRadius:
-                    BorderRadius.circular(15),
-                child: Container(
-                  width: 53,
-                  height: 53,
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.circular(15),
-                    border: Border.all(
-                      color: const Color(
-                        0xFF292932,
-                      ),
-                    ),
-                  ),
-                  child: const Icon(
-                    Icons
-                        .tune_rounded,
-                    color: Colors.white,
-                    size: 21,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ],
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        textDirection: TextDirection.rtl,
+        style: const TextStyle(color: Colors.white, fontSize: 13, fontWeight: FontWeight.w600),
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: const TextStyle(color: Colors.white38, fontSize: 12),
+          prefixIcon: const Icon(Icons.search_rounded, color: Color(0xFFFF176F), size: 23),
+          filled: true,
+          fillColor: const Color(0xFF101925),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(18), borderSide: const BorderSide(color: Color(0xFF26364A))),
+          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(18), borderSide: const BorderSide(color: Color(0xFF26364A))),
+          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(18), borderSide: const BorderSide(color: Color(0xFFFF176F), width: 1.4)),
+        ),
       ),
     );
   }
